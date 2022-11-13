d77a475c5d2c0e7a0d0a7a0824744f3649dd3b11
amo-tech <kaomi.x1003@gmail.com>
2022-11-13 16:53:46 +0900

    component

--- 04_react_basic/src/070_component_nest/start/Example.js
+++ 04_react_basic/src/070_component_nest/start/Example.js
@@ -1,9 +1,11 @@
 import "./Example.css";
+import {List} from "../start/components/List";
 
 const Example = () => {
   return (
     <div className="component">
       <h3>Hello Component</h3>
+      <List />
     </div>
   );
 };
