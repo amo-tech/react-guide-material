bb1ac218a1d0b0ed05d1efd7ad701420817f832b
amo-tech <kaomi.x1003@gmail.com>
2022-11-15 00:19:32 +0900

    jsx続き

--- 04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
+++ 04_react_basic/src/080_expr_in_jsx/start/components/Expression.js
@@ -1,4 +1,18 @@
+import "./Expression.css";
+
 export const Expression = () => {
     const title = "Expression";
-    return <h3>Hello {title}</h3>
+    const arry = [ "item1", "item2", "item3"];
+    const Hello = (arg) => `${arg} Function`
+    const jsx = <h3>PPP</h3>;
+    console.log(jsx)
+    return (
+        <div className={title.toLocaleLowerCase()}>
+            <h3>Hello {title}</h3>
+            <h3>{arry}</h3>
+            <h3>{Hello("World")}</h3>
+            {<h3>AAA</h3>}
+            {jsx}
+        </div>
+    );
 };
