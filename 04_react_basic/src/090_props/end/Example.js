d306fcd50c60b3d1f5a0713e7a1fde5b5898f88b
amo-tech <kaomi.x1003@gmail.com>
2022-11-16 22:36:01 +0900

    props

--- 04_react_basic/src/090_props/end/Example.js
+++ 04_react_basic/src/090_props/end/Example.js
@@ -2,21 +2,21 @@ import Child from "./components/Child";
 
 const Example = () => {
     const hello = (arg) => `Hello ${arg}`;
-    // const o = {
-    //     color: "red",
-    //     num: 123
-    // }
+    const o = {
+        color: "red",
+        num: 123
+    }
     return (
         <>
             <Child 
                 // POINT propsには全てのタイプの値を渡すことができます。
-                // {...o}
+                {...o}
                 color="blue"
                 fn={hello}
                 bool
                 obj={{ name: 'Tom', age: 18 }}
             />
-            <Child color="red" />
+            {/* <Child color="red" /> コンポーネントを書いていないものはエラーになるかundefinedになる*/}
         </>
     )
 };
--- 04_react_basic/src/090_props/end/components/Child.js
+++ 04_react_basic/src/090_props/end/components/Child.js
@@ -9,15 +9,15 @@ import "./Child.css";
 /* POINT Propsのデフォルト値を設定する！ */
 const Child = ({ color: c = 'green', num, fn, bool, obj }) => {
 
-  // console.log(bool);
-
+  console.log(fn);
+  
   return (
     <div className={`component ${c}`}>
       <h3>Hello Component</h3>
-      {/* <h3>{num}</h3>
+      <h3>{num}</h3>
       <h3>{fn('Props')}</h3>
       <h3>{ bool ? 'true' : 'false'}</h3>
-      <h3>{ obj.name + obj.age }</h3> */}
+      <h3>{ obj.name + obj.age }</h3>
     </div>
   );
 };
