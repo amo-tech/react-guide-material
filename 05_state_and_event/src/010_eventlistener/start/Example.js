dc9c9713225f2c04e43241510ace0282110669ea
amo-tech <kaomi.x1003@gmail.com>
2022-11-21 23:52:22 +0900

    eventlisner

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,16 @@
 const Example = () => {
+  const clickHandler = () => {
+    alert("clicked");
+  };
+  const clickHandler2 = () => {
+    console.log("clicked")
+  };
   return (
-    <></>
+    <>
+      {/* clickHandlerに()付けると実行結果が帰ってくる　return文がないものはundefined */}
+      <button onClick={clickHandler}>Click!</button> 
+      <button onClick={clickHandler2}>Click!</button>
+    </>
   );
 };
 
