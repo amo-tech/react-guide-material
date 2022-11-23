c504d3937138b4865dcd465850b645af21ac0b05
amo-tech <kaomi.x1003@gmail.com>
2022-11-23 20:49:55 +0900

    useState

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,8 +1,12 @@
-const Example = () => {
+import { useState } from "react";
 
+const Example = () => {
+  const [val, setVal] = useState(); // 0:参照用の値、1:更新用の関数
   return (
     <>
-      
+      <input type="text" onChange={(e) => {
+        setVal(e.target.value)
+      }} /> = {val}
     </>
   );
 };
