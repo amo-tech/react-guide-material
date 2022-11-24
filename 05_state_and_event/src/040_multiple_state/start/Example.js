4f2a61173247c38586bdc7bc27435cf871619f28
amo-tech <kaomi.x1003@gmail.com>
2022-11-24 12:44:59 +0900

    useState

--- 05_state_and_event/src/040_multiple_state/start/Example.js
+++ 05_state_and_event/src/040_multiple_state/start/Example.js
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const Example = () => {
-  
+    console.log(<Example/>)
+    const [countA, setCountA] = useState(0);
+    const [countB, setCountB] = useState(5);
+    const [countC, setCountC] = useState(10);
+  return(
+      <>
+        <p>ボタンAを{countA}回押しました！</p>
+        <button onClick={() => {
+            setCountA(countA + 1);
+        }}>buttonA</button>
+        <p>ボタンを{countB}回押しました！</p>
+        <button onClick={() => {
+            setCountB(countB + 1);
+        }}>buttonB</button>
+        <p>ボタンCを{countC}回押しました！</p>
+        <button onClick={() => {
+            setCountC(countC + 1);
+        }}>buttonC</button>
+      </>
+  );
 };
 
 export default Example;
--- 05_state_and_event/src/050_prev_state/start/Example.js
+++ 05_state_and_event/src/050_prev_state/start/Example.js
@@ -1,7 +1,21 @@
 import { useState } from "react";
 
 const Example = () => {
-  
+    const [ count, setCount ] = useState(0)
+    const add = () => {
+        setCount(count + 1);
+        setCount(prevState => prevState + 1);
+    };
+    const minus = () => {
+        setCount(count - 1);
+    };
+  return(
+      <>
+        <p>現在のカウント数:{count}</p>
+        <button onClick={ add }>+</button>
+        <button onClick={ minus }>-</button>
+      </>
+  );
 };
 
 export default Example;
