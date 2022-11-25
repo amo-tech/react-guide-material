bb765712694411b5095532a8ae09d40a3d798cde
amo-tech <kaomi.x1003@gmail.com>
2022-11-25 23:55:38 +0900

    object2

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
-  const [person, setPerson] = useState()
+  const [person, setPerson] = useState({ name: "TOM", age: 18});
   return(
     <>
+      <h3>Name:{person.name}</h3>
+      <h3>Age:{person.name}</h3>
     </>
   )
 };
