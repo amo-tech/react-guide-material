8a8229bc14057830649e0cce02ccb5ef2cfbe718
amo-tech <kaomi.x1003@gmail.com>
2022-11-25 23:58:02 +0900

    object3

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -3,10 +3,15 @@ import { useState } from "react";
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
   const [person, setPerson] = useState({ name: "TOM", age: 18});
+
+  const chageName = () => {
+
+  };
   return(
     <>
       <h3>Name:{person.name}</h3>
-      <h3>Age:{person.name}</h3>
+      <h3>Age:{person.age}</h3>
+      <input type="text" onChange={chageName}/>
     </>
   )
 };
