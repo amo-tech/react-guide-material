e9e622cac68a9a11f33a5c8bdbab720fcad5a992
amo-tech <kaomi.x1003@gmail.com>
2022-11-25 23:53:24 +0900

    object

--- 05_state_and_event/src/060_state_object/start/Example.js
+++ 05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState()
+  return(
+    <>
+    </>
+  )
 };
 
 export default Example;
