8837ce4963f41090b61aa7cd9572656994e12812
amo-tech <kaomi.x1003@gmail.com>
2022-12-03 22:18:41 +0900

    100single_checkbox

--- 06_control_and_form/src/100_single_checkbox/start/Example.js
+++ 06_control_and_form/src/100_single_checkbox/start/Example.js
@@ -1,10 +1,21 @@
+import {useState} from "react";
+
 const Example = () => {
+  const [isChecked, setIsChecked] = useState(true);
+  const toggleChecked = (e) => {
+    setIsChecked(prevstate => !prevstate);
+  }; 
   return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
+    <>
+      <label htmlFor="my-checkbox">check: </label>
+      <input
+        id="my-checkbox"
+        type="checkbox" 
+        checked={isChecked}
+        onChange={(e)=>toggleChecked(e)}
+      />
+      <div>{isChecked ? "ON!" : "OFF!"}</div>
+    </>
   );
 };
 
