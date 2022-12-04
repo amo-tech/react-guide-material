c0cdccc9f01712a519a8ff093f6068037152a3f8
amo-tech <kaomi.x1003@gmail.com>
2022-12-04 23:50:14 +0900

    110multi_checkbox

--- 06_control_and_form/src/110_multi_checkbox/start/Example.js
+++ 06_control_and_form/src/110_multi_checkbox/start/Example.js
@@ -9,9 +9,36 @@ const Example = () => {
 
   const [sum, setSum] = useState(0);
 
+  const handleChange = (e) => {
+    
+    const newFruits = fruits.map(fruit => {
+      const newFruit = { ...fruit }
+      if(newFruit.label === e.target.value){
+        newFruit.checked = !fruits.checked;
+      }
+      return newFruit;
+    })
+    setFruits(newFruits);
+
+    let sumVal = 0;
+    newFruits.forEach(fruit => {
+      if(fruit.checked){
+        sumVal += fruit.value
+      }
+    })
+    setSum(sumVal);
+  };
+
   return (
     <div>
-      
+      {fruits.map(fruits => {
+        return(
+          <div key={fruits.label}>
+          <input id={fruits.label} type="checkbox" value={fruits.label} checked={fruits.checked} onChange={handleChange} />
+          <label htmlFor={fruits.label}>{fruits.label}:{fruits.value}</label>
+          </div>
+        )
+      })}
       <div>合計：{sum}</div>
     </div>
   );
