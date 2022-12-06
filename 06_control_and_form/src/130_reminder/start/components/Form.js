cb646a84fbecc256d51c74b5d110725f4ad66850
amo-tech <kaomi.x1003@gmail.com>
2022-12-06 23:50:16 +0900

    130remindert

---
+++ 06_control_and_form/src/130_reminder/start/components/Form.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const Form = ({createTodo}) => {
+    const [enteredTodo, setEnteredTodo] = useState("");
+    const addTodo = (e) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 1e5),
+            content: enteredTodo,
+        };
+        createTodo(newTodo);
+    };
+    return(
+        <div>
+            <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
+            <button onClick={addTodo}>Add</button>
+        </div>
+    )
+}
+
+export default Form;
