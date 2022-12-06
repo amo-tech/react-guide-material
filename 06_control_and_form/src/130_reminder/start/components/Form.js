349454db6af54dab847e243eed38e6553b974e9b
amo-tech <kaomi.x1003@gmail.com>
2022-12-06 23:57:13 +0900

    130remindert2

--- 06_control_and_form/src/130_reminder/start/components/Form.js
+++ 06_control_and_form/src/130_reminder/start/components/Form.js
@@ -3,16 +3,20 @@ import { useState } from "react";
 const Form = ({createTodo}) => {
     const [enteredTodo, setEnteredTodo] = useState("");
     const addTodo = (e) => {
+        e.preventDefault();
         const newTodo = {
             id: Math.floor(Math.random() * 1e5),
             content: enteredTodo,
         };
         createTodo(newTodo);
+        setEnteredTodo("");
     };
     return(
         <div>
-            <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
-            <button onClick={addTodo}>Add</button>
+            <form onSubmit={addTodo}>
+                <input type="text" value={enteredTodo} onChange={(e) => setEnteredTodo(e.target.value)} />
+                <button>Add</button>
+            </form>
         </div>
     )
 }
