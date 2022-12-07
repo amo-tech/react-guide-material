d9a11a4520d560cfff930d0fbd863bc810bfb0af
amo-tech <kaomi.x1003@gmail.com>
2022-12-07 23:59:19 +0900

    010inline

--- 07_styling_component/src/010_inline_style/start/Example.js
+++ 07_styling_component/src/010_inline_style/start/Example.js
@@ -4,11 +4,22 @@ const Example = () => {
     const [isSelected, setIsSelected] = useState(false);
 
     const clickHandler = () => setIsSelected(prev => !prev);
+    const style = {
+        margin: "auto",
+        width: 120,
+        height: 60,
+        display: "blocked",
+        fontWeight: "bold",
+        borderRadius: 9999,
+        cursor: "pointer",
+        border: "none",
+        background: isSelected ? "pink" : ""
+    };
 
     return (
         <>
-            <button onClick={clickHandler}>ボタン</button>
-            <div>{isSelected && "クリックされました。"}</div>
+            <button style={style} onClick={clickHandler}>ボタン</button>
+            <div style={{ textAlign: "center" }}>{isSelected && "クリックされました。"}</div>
         </>
     )
 };
