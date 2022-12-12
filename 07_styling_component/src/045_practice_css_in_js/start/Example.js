52ff03dc09e4ed788ac79d0e0ab5943b7e46e0f8
amo-tech <kaomi.x1003@gmail.com>
2022-12-12 22:52:01 +0900

    045practiceCSS

--- 07_styling_component/src/045_practice_css_in_js/start/Example.js
+++ 07_styling_component/src/045_practice_css_in_js/start/Example.js
@@ -7,7 +7,18 @@ const FirstButton = styled.button`
   margin: 0.5rem 1rem;
   width: 11rem;
   border: none;
+  background-color: pink;
 `;
+
+const SecondButton = styled(FirstButton)`
+  color: white;
+  background-color: red;
+`;
+
+const ThirdButton = styled(SecondButton)`
+  background-color: ${(props) => props.dark ? "black" : "green"};
+`;
+
 
 const Example = () => {
   return (
@@ -28,6 +39,8 @@ const Example = () => {
         </ul>
       </p>
       <FirstButton>ボタン1</FirstButton>
+      <SecondButton>ボタン2</SecondButton>
+      <ThirdButton>ボタン3</ThirdButton>
     </>
   );
 };
