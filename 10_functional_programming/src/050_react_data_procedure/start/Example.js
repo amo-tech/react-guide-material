6c0b5e2c926344f62b7838f0247f8de08a6e41b3
amo-tech <kaomi.x1003@gmail.com>
2022-12-27 09:00:43 +0900

    050reactDataProcedure

--- 10_functional_programming/src/050_react_data_procedure/start/Example.js
+++ 10_functional_programming/src/050_react_data_procedure/start/Example.js
@@ -1,6 +1,17 @@
 import { useState } from "react";
 
+/* 
+POINT Reactでの状態管理(state)と処理(関数コンポーネント)の分離
+
+関数型（純粋関数）
+・fn(決まった引数) : propsとstateに依存
+  -> 決まった戻り値 : JSX
+*/
+
+let value = 0;
 const Example = () => {
+
+  // 関数コンポーネントはJSXを作成する場所なので、それ以外の処理は書かない
   const [ state, setState ] = useState(0);
   const increment = () => {
     setState(state + 1);
